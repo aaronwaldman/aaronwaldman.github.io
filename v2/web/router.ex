40258dd5edde2cb6a6651114300e9af228bc2cc8
Adam Chamberland <achamberland@kayak.com>
2016-12-31 16:00:54 -0500

    Add instagram link in header and add way too many breakpoints to prevent header wrapping at breakpoints

---
+++ v2/web/router.ex
@@ -0,0 +1,36 @@
+defmodule V2.Router do
+  use V2.Web, :router
+
+  pipeline :browser do
+    plug :accepts, ["html"]
+    plug :fetch_session
+    plug :fetch_flash
+    plug :protect_from_forgery
+    plug :put_secure_browser_headers
+  end
+
+  pipeline :api do
+    plug :accepts, ["json"]
+  end
+
+  scope "/", V2 do
+    pipe_through :browser # Use the default browser stack
+
+    get "/", PageController, :index
+
+    resources "/shows", ShowsController, only: [:index, :show]
+    resources "/contact", ContactController, except: [:delete]
+  end
+
+  scope "/admin", V2.Admin, as: :admin do
+    pipe_through :browser
+
+    resources "/shows", ShowsController
+    resources "/contact", ContactController
+  end
+
+  # Other scopes may use custom stacks.
+  # scope "/api", V2 do
+  #   pipe_through :api
+  # end
+end
